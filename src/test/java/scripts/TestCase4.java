b069a1a16a98c2c601c60601af84c213d045a071
Muhammad <Muhammad@DESKTOP-O2MGJE7>
2023-01-06 16:36:18 +0530

    updte

---
+++ src/test/java/scripts/TestCase4.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase4 {
+@Test
+public void tc4()
+{
+	Reporter.log("hello TestNG ",true);
+}
+}
